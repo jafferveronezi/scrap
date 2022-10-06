6260eacfd4ea55a4bbf03d3035e9ddc37cbb5e11
Jaffer Veronezi <veronezitecnologia@gmail.com>
2022-10-05 22:08:43 -0300

    make it better

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/books/:page', (req, res) => {
         })
 })
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 app.listen (port , () => {
     console.log(`Servido rodando em http://localhost:${port}/books/1`)
 })
